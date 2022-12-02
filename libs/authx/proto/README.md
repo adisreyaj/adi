530a66dd95e1e521ddf0bf190628148f7f90fba2
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-12-02 21:44:09 +0530

    feat(auth): add proto

---
+++ libs/authx/proto/README.md
@@ -0,0 +1,11 @@
+# authx-proto
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build authx-proto` to build the library.
+
+## Running unit tests
+
+Run `nx test authx-proto` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/authx/proto/src/lib/auth/auth.types.ts
@@ -0,0 +1,50 @@
+import { Observable } from 'rxjs';
+
+export const AUTH_SERVICE = 'AuthService';
+export const AUTH_PACKAGE = 'auth';
+
+export interface AuthServiceClient {
+  register(request: RegisterRequest): Observable<RegisterResponse>;
+
+  login(request: LoginRequest): Observable<LoginResponse>;
+
+  validate(request: ValidateRequest): Observable<ValidateResponse>;
+}
+
+export enum AuthMethod {
+  Register = 'Register',
+  Login = 'Login',
+  Validate = 'Validate',
+}
+
+export interface RegisterRequest {
+  email: string;
+  password: string;
+}
+
+export interface RegisterResponse {
+  status: number;
+  error: string[];
+  token: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  status: number;
+  error: string[];
+  token: string;
+}
+
+export interface ValidateRequest {
+  token: string;
+}
+
+export interface ValidateResponse {
+  status: number;
+  error: string[];
+  userId: number;
+}
